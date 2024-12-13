9cdb857f743fb186d7d10a94258778e61da86937
AnonymousPC <shyeyian@petalmail.com>
2024-12-14 01:34:26 +0800

    fix windows cplus include path

--- main.cpp
+++ main.cpp
@@ -1,31 +1,15 @@
-#include "test.cpp"
-// #include "generic/basic/interface.hpp"
-// #include "generic/container/interface.hpp"
-// #include "generic/io/interface.hpp"
-// #include "generic/math/interface.hpp"
-// #include "specific/audio/interface.hpp"
-// #include "specific/neural/interface.hpp"
-// #include "specific/spirit/interface.hpp"
-// #include "specific/stock/interface.hpp"
+#include "generic/basic/interface.hpp"
+#include "generic/container/interface.hpp"
+#include "generic/io/interface.hpp"
+#include "generic/math/interface.hpp"
+#include "specific/audio/interface.hpp"
+#include "specific/neural/interface.hpp"
+#include "specific/spirit/interface.hpp"
+#include "specific/stock/interface.hpp"
 
 // using namespace ap;
 
 int main ( )
 {
-    
+    http_get("https://www.google.com").each(print);
 }
-
-/*
-702
-: 
-"error_info.focus"
-703
-: 
-"error_info.removeView"
-704
-: 
-"error_info.resetViewLocation"
-705
-: 
-"error_info.toggleVisibility"
-*/
