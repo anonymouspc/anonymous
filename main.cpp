d4c21366829b46798c3213487d3fed2119e133f3
AnonymousPC <shyeyian@petalmail.com>
2024-12-14 01:20:24 +0800

    sync

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include "test.cpp"
-#include "generic/basic/interface.hpp"
-#include "generic/container/interface.hpp"
-#include "generic/io/interface.hpp"
-#include "generic/math/interface.hpp"
-#include "specific/audio/interface.hpp"
-#include "specific/neural/interface.hpp"
-#include "specific/spirit/interface.hpp"
-#include "specific/stock/interface.hpp"
+// #include "generic/basic/interface.hpp"
+// #include "generic/container/interface.hpp"
+// #include "generic/io/interface.hpp"
+// #include "generic/math/interface.hpp"
+// #include "specific/audio/interface.hpp"
+// #include "specific/neural/interface.hpp"
+// #include "specific/spirit/interface.hpp"
+// #include "specific/stock/interface.hpp"
 
-using namespace ap;
+// using namespace ap;
 
 int main ( )
 {
