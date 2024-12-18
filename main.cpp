571f16b079d6687db3cb823b3f99bb2fa7ac2bb1
AnonymousPC <shyeyian@petalmail.com>
2024-12-18 10:18:17 +0800

    sync

--- main.cpp
+++ main.cpp
@@ -6,13 +6,42 @@
 #include "specific/neural/interface.hpp"
 #include "specific/spirit/interface.hpp"
 #include "specific/stock/interface.hpp"
-#include <vector>
 
 using namespace ap;
 
+struct noisy
+{
+    noisy() = default;
+    noisy(const noisy&)            { print("copy construct"); }
+    noisy(noisy&&)                 { print("move construct"); }
+    noisy& operator=(const noisy&) { print("copy assign"); return self; }
+    noisy& operator=(noisy&&)      { print("move assign"); return self; }
+};
+
+struct my_iterator
+{
+    int val = 0;
+
+    my_iterator() = default;
+    my_iterator(int v) extends val(v) {};
+   ~my_iterator() { print("~iterator"); }
+    
+    int& operator*() { return val; };
+    my_iterator& operator++()    { ++val; return self; }
+    my_iterator  operator++(int) { let it = self; ++self; return it; }
+
+    friend bool operator == (const my_iterator&, const my_iterator&) = default;
+};
+
+struct my_class
+{
+    auto begin ( ) const { return my_iterator(0); };
+    auto end   ( ) const { return my_iterator(10); };
+};
+
 int main ( )
 {
-    array a = { {1,2}, {3,4} };
-    a[1];       // array&;
-    a[1,2], // array&;
+    my_class obj;
+    for ( auto& o in obj )
+        print(&o);
 }
