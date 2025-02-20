48bc2ecfb57c243ee6c7642b5b1e4bb91230afc6
AnonymousPC <shyeyian@petalmail.com>
2025-02-20 12:52:12 +0800

    ssl certificate

--- generic/io/pipe/pipe.hpp
+++ generic/io/pipe/pipe.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 // Include
-
-#include "pipe_base/pipe_buf.hpp"
-#include "pipe_base/pipe_stream.hpp"
-#include "third_party/third_party.hpp"
+#ifndef _WIN32
+    #include "pipe_base/pipe_buf.hpp"
+    #include "pipe_base/pipe_stream.hpp"
+#endif
