395eece06cc882a0a3ea7ce1084117564a4006db
AnonymousPC <shyeyian@petalmail.com>
2025-04-29 15:15:20 +0800

    before array

--- include/anonymous/container/polymorphic/polymorphic_class/function.hpp
+++ include/anonymous/container/polymorphic/polymorphic_class/function.hpp
@@ -7,6 +7,11 @@ template < class ret_type, class... arg_types, class device >
 class function<ret_type(arg_types...),device>
     extends public device::template function<ret_type(arg_types...)>
 {
+    public: // Typedef
+        using return_type   = ret_type;
+        template < int index > requires ( ( index >= -sizeof...(arg_types) and index <= -1 ) or ( index >= 1 and index <= sizeof...(arg_types) ) ) using argument_type = index_type_of<index,arg_types...>;
+        using device_type   = device;
+
     public: // Core
         constexpr function ( )                              = default;
         constexpr function ( const function&  )             = default;
